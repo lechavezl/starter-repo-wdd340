789032eff529112c9efc3716b99095b92d4a94e5
lechavezl <luisernestochavezleal@gmail.com>
2023-10-17 18:10:22 -0400

    Functions to handle errors.

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
-const utilities = require("./utilities/index")
+const utilities = require("./utilities/")
 
 
 /* ***********************
@@ -27,7 +27,8 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 //Index route
-app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
+
 
 // Inventory routes
 app.use("/inv", inventoryRoute)
@@ -45,14 +46,15 @@ app.use(async (req, res, next) => {
 * Place after all other middleware
 *************************/
 app.use(async (err, req, res, next) => {
-  let nav = await utilities.getNav();
-  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
-    message: err.message,
+    message,
     nav
-  });
-});
+  })
+})
 
 
 /* ***********************
